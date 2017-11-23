eca4a26badc9b31e7b7549e6e6b030765adc3559
lee jung uk <dlwjddnr93@naver.com>
2017-11-23 19:42:00 +0900

    NGPCODE

---
+++ NGP TermProject_ver_0_2/Simplegame/SceneMgr.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include "Object.h"
+#include "Renderer.h"
+// 길찾기 알고리즘 등 여러가지가 들어감
+// 이 클래스가 뚱뚱해지고 내용이 많아질 것임.
+
+#define MAX_OBJECTS_COUNT 300
+
+#define OBJECT_CHARACTER 0
+#define OBJECT_BUILDING 1
+#define OBJECT_BULLET 2
+#define OBJECT_ARROW 3
+
+
+
+class SceneMgr
+{
+
+public:
+	SceneMgr(int x , int y);
+	~SceneMgr();
+
+	// 오브젝트 생성
+	int CreateObject(float x, float y, int objectType, int teamType);
+	void DeleteObject(int index);
+	// 오브젝트 업데이트, 겟, 
+	void UpdateAllObject(float elpasedTime);
+	void DrawAllObject();
+	// Get();
+	int GetObjectCnt() { return MAX_OBJECTS_COUNT; }
+	Object* GetActorObject(int index);
+
+	//Set()
+	 bool BoxColisionTest(Object* a, Object* b);
+	 void DoColisionTest();
+
+private:
+
+	Renderer* m_Renderer;
+	int prevTime;
+	int currentTime;
+
+	float enemyCooltime = 0.f;
+
+	int objectType;
+
+	GLuint m_characterTexture = 0;
+	GLuint m_bulletTexture = 0;
+
+public:
+	Object *m_objects[MAX_OBJECTS_COUNT];
+
+};
+
---
+++ NGP TermProject_ver_0_2/Simplegame/SimpleGame.cpp
@@ -0,0 +1,158 @@
+/*
+Copyright 2017 Lee Taek Hee (Korea Polytech University)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the What The Hell License. Do it plz.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY.
+*/
+
+#include "stdafx.h"
+#include <iostream>
+#include "Dependencies\glew.h"
+#include "Dependencies\freeglut.h"
+
+#include "SceneMgr.h"
+
+
+SceneMgr *g_SceneMgr = NULL;
+
+DWORD g_prevTime = 0;
+
+bool g_LButtonDown = false;
+float g_mouseCooltime = 0.f;
+
+void RenderScene(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+	DWORD currTime = timeGetTime();
+	DWORD elapsedTime = currTime - g_prevTime;
+	g_prevTime = currTime;
+	g_mouseCooltime += elapsedTime * 0.001f;
+	std::cout << " mouse cooltime : " << g_mouseCooltime << std::endl;
+
+	g_SceneMgr->UpdateAllObject(float(elapsedTime));
+	g_SceneMgr -> DrawAllObject();
+
+	glutSwapBuffers();
+}
+
+void Idle(void)
+{
+	// Renderer Test
+
+	RenderScene();
+}
+
+// button
+// GLUT_LEFT_BUTTON , GLUT_MIDDLE_BUTTON , GLUT_RIGHT_BUTTON
+// state
+// GLUT_UP , GLUT_DOWN
+// 클릭이나 드래그 같은것이 구현되지 않아서 직접 구현 해야 함.
+void MouseInput(int button, int state, int x, int y)
+{
+	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
+		g_LButtonDown = true;
+
+	if (button == GLUT_LEFT_BUTTON && state == GLUT_UP) {
+		if (g_LButtonDown) {
+			if (g_mouseCooltime >= 0.f && (WINDOWHALFSIZE_HEIGHT)-y < 0.f)
+			{
+				g_SceneMgr->CreateObject(x - (WINDOWHALFSIZE_WIDTH), (WINDOWHALFSIZE_HEIGHT)-y, OBJECT_CHARACTER, TEAM_1);
+				g_mouseCooltime = 0.f;
+			}
+		}
+		g_LButtonDown = false;
+	}
+	RenderScene();
+}
+
+void KeyInput(unsigned char key, int x, int y)
+{
+	switch (key)
+	{
+		case GLUT_KEY_UP:
+		{
+			//g_SceneMgr->GetActorObject()
+			for (int i = 0; i < g_SceneMgr->GetObjectCnt(); ++i)
+			{
+				if (g_SceneMgr->m_objects[i]->GetType() == OBJECT_CHARACTER)
+					g_SceneMgr->m_objects[i]->SetMovePositionX(5.0f);
+			}
+
+		}
+		break;
+		case GLUT_KEY_DOWN:
+		{
+
+		}
+		break;
+		case GLUT_KEY_LEFT:
+		{
+
+		}
+		break;
+		case GLUT_KEY_RIGHT:
+		{
+
+		}
+		break;
+	}
+	RenderScene();
+}
+ 
+void SpecialKeyInput(int key, int x, int y)
+{
+	RenderScene();
+}
+
+int main(int argc, char **argv)
+{
+	// Initialize GL things
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowPosition(0, 0);
+	glutInitWindowSize(WINDOWSIZE_WIDTH, WINDOWSIZE_HEIGHT);
+	glutCreateWindow("Game Software Engineering KPU");
+
+	glewInit();
+	if (glewIsSupported("GL_VERSION_3_0"))
+	{
+		std::cout << " GLEW Version is 3.0\n ";
+	}
+	else
+	{
+		std::cout << "GLEW 3.0 not supported\n ";
+	}
+
+
+	glutDisplayFunc(RenderScene);
+	glutIdleFunc(Idle);
+	glutKeyboardFunc(KeyInput);
+	glutMouseFunc(MouseInput);
+	glutSpecialFunc(SpecialKeyInput);
+
+	//Initialize Renderer
+
+   g_SceneMgr = new SceneMgr(WINDOWSIZE_WIDTH, WINDOWSIZE_HEIGHT);
+   //for (int i = 0 ; i< 200; i++)
+
+   g_SceneMgr->CreateObject(0, 0, OBJECT_CHARACTER, TEAM_2);
+
+   for (int i = 0; i < 50; ++i)
+   {
+	   g_SceneMgr->CreateObject(0, 0, OBJECT_ARROW, TEAM_1);
+   }
+
+	g_prevTime = timeGetTime();
+
+	glutMainLoop();
+
+	delete g_SceneMgr;
+
+    return 0;
+}
+

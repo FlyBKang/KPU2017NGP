247284625b481f16fc931198e5b125d8b509a594
lee jung uk <dlwjddnr93@naver.com>
2017-11-23 19:49:29 +0900

    171123

---
+++ NGP TermProject_ver_0_2/Simplegame/Packet.h
@@ -0,0 +1,36 @@
+#pragma once
+
+//
+#include "SceneMgr.h"
+#include "Object.h"
+struct Playerpacket
+{
+	int PlayerNum;
+	float PlayerColor[3];
+	float PlayerPosition[2];
+	int     PlayerLife;
+	int   PlayerScore;
+	bool  isCollide;
+};
+
+struct TimePacket
+{
+	float time;
+};
+
+struct End
+{
+	Object* player;
+	float Endtime;
+	bool isEnd;
+};
+struct KeyState
+{
+	bool isKeyUp;
+	bool isKeyLeft;
+	bool isKeyDown;
+	bool isKeyRight;
+};
+//Playerpacket *ToSeverPlayerPacket;
+//ToSeverPlayerPacket->PlayerColor[0] = g_SceneMgr->m_objects[0]->GetColorRed;
+//ToSeverPlayerPacket
